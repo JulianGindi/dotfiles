5df2351842f5218b728cfc7e7fc541eb79c9a8d9
Julian Gindi <_@gindi.io>
2017-09-01 13:07:34 -0700

    finished first take at setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,26 @@
 ln -s tmux/tmux.conf $HOME/.tmux.conf
 ln -s vim/vimrc $HOME/.vimrc
 ln -s zsh/zshrc $HOME/.zshrc
+ln -s git/gitconfig $HOME/.gitconfig
 
 # Change default shell
 chsh -s $(which zsh)
+
+# Removing old vim package manager and reinstall
+rm -rf vim/Vundle.vim
+
+# Clone new Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/dotfiles/vim/Vundle.vim
+
+# Install vim plugins
+vim +PluginInstall +qall
+
+# Installing some needed packages to make everything work nicely
+sudo apt-get install python python-pip python3
+sudo apt-get install python-virtualenv
+
+# Install virtualenvwrapper with pip
+pip install virtualenvwrapper
+
+# Refresh zsh
+exec zsh
