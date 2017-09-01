6b30571f24bc02c68b1c912dcaa7f386a09728ea
Julian Gindi <_@gindi.io>
2017-09-01 13:10:19 -0700

    using absolute paths ftw

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 # Create links
 
-ln -s tmux/tmux.conf $HOME/.tmux.conf
-ln -s vim/vimrc $HOME/.vimrc
-ln -s zsh/zshrc $HOME/.zshrc
-ln -s git/gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
 
 # Change default shell
 chsh -s $(which zsh)
