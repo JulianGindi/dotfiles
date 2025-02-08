5b53c6da173790a0bc86f375340d3dfd8cda862d
Julian Gindi <julian@gindi.io>
2025-02-07 16:27:32 -0800

    trying to get better lazy loading

--- nvim/lua/config/plugins/org.lua
+++ nvim/lua/config/plugins/org.lua
@@ -2,6 +2,8 @@ return {
 	"nvim-neorg/neorg",
 	dependencies = { "luarocks.nvim" },
 	-- put any other flags you wanted to pass to lazy here!
+	cmd = "Neorg",
+	ft = "norg",
 	config = function()
 		require("neorg").setup({
 			load = {
