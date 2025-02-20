3cdc8e3a1eeb967d225d8bdac3343c8f16faca52
Julian Gindi <julian@gindi.io>
2025-02-19 22:31:15 -0800

    some tweeks

--- nvim/lua/config/config.lua
+++ nvim/lua/config/config.lua
@@ -12,10 +12,13 @@ o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
 o.title = true -- When on, the title of the window will be set to the value of 'titlestring'
 o.showmatch = true -- When a bracket is inserted, briefly jump to the matching one
 
--- Keyboard mappings
+-- General Keyboard mappings
 local keymap = vim.keymap
 keymap.set("n", "<leader>m", "<cmd>Neorg index<cr>")
 keymap.set("n", "<leader>1", "<cmd>Neorg journal today<cr>")
 
 -- Tab creation and navigation
 keymap.set("n", "<leader>t", "<cmd>:tabnew<cr>")
+
+-- Clearning "highlights"
+keymap.set("n", "<leader>/", "<cmd>:noh<cr>")
