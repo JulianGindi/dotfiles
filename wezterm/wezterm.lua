fc9761cdde8082f2343f5892062cdbf8580788dc
Julian Gindi <julian@gindi.io>
2025-05-22 09:00:05 -0700

    added tab renaming and working on minos notes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@
 local wezterm = require("wezterm")
 local appearance = require("appearance")
 local keyboard = require("keyboard")
+local tabs = require("tabs")
 
 local os_name = wezterm.target_triple
 
@@ -11,6 +12,9 @@ appearance.setup_main(config)
 
 -- Setup keyboard shortcuts
 keyboard.setup_keys(config)
+
+-- Setup tab renaming
+tabs.setup_rename(config)
 
 -- Events
 wezterm.on("update-status", appearance.draw_right_status)
