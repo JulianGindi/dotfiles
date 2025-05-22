3591411b4db45b716e56a231228a9e191be416b7
Julian Gindi <julian@gindi.io>
2025-05-22 09:26:41 -0700

    initial commit of minos notes work

--- nvim/lua/config/minos.lua
+++ nvim/lua/config/minos.lua
@@ -1,1 +1,24 @@
-local note_path = "~/Desktop/notes"
+local mod = {}
+
+-- Function to get and print current working directory
+function mod.print_cwd()
+	local cwd = vim.fn.getcwd()
+	print("Current working directory: " .. cwd)
+end
+
+local function setup_keybinding()
+	vim.keymap.set("n", "<localleader>m", mod.print_cwd, {
+		desc = "Print current working directory",
+		noremap = true,
+		silent = true,
+	})
+end
+
+function mod.setup()
+	setup_keybinding()
+end
+
+-- Auto-setup when the module is loaded
+mod.setup()
+
+return mod
