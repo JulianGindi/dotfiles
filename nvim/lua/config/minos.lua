ace9f7489f06db1258da94ebf2672f99744b976f
Julian Gindi <julian@gindi.io>
2025-05-28 09:30:29 -0700

    some initial code for minos notes actually working

--- nvim/lua/config/minos.lua
+++ nvim/lua/config/minos.lua
@@ -1,21 +1,46 @@
 local mod = {}
 
--- Function to get and print current working directory
-local function print_cwd()
-	local cwd = vim.fn.getcwd()
-	print("Current working directory: " .. cwd)
+local notes_dir = "/Users/jgindi/Desktop/temp_notes/"
+
+
+local function new_note(name)
+  local note_template = { "# Note -- " .. name }
+  local note_file = notes_dir .. name .. ".minos"
+  vim.fn.writefile(note_template, note_file)
+end
+
+local function create_index_file()
+	local index_template = { "# Minos Notes Index" }
+	local index_file = notes_dir .. "index.minos"
+	if vim.fn.filereadable(index_file) == 0 then
+		vim.fn.writefile(index_template, index_file)
+		print("Index file created with Minos Notes")
+	else
+		print("File already existed")
+	end
+end
+
+local function create_note_mapping(note_name)
+  return function()
+    new_note(note_name)
+  end
 end
 
 local function setup_keybinding()
-	vim.keymap.set("n", "<localleader>m", print_cwd, {
-		desc = "Print current working directory",
+	vim.keymap.set("n", "<localleader>m", create_note_mapping("test"), {
+		desc = "Create a new Minos note",
 		noremap = true,
 		silent = true,
 	})
 end
 
+local function test_ui()
+	-- :h prompt-buffer
+end
+
 function mod.setup()
 	setup_keybinding()
+	create_index_file()
 end
 
 -- Auto-setup when the module is loaded
